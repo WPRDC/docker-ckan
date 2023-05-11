240330a774f0c87eb901ac17fc0f312d191e10d6
Steve Saylor <steven.saylor@pitt.edu>
2023-05-11 14:49:23 -0400

    added example data in ckan build process

---
+++ ckan/docker-entrypoint.d/10-preopulate.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+
+orgs_len=$(ckanapi action organization_list | jq '. | length')
+groups_len=$(ckanapi action group_list | jq '. | length')
+datasets_len=$(ckanapi action package_list | jq '. | length')
+
+# Organizations
+if [ "$orgs_len" -lt 5 ]; then
+  ckanapi load organizations -I /tmp/fixtures/organizations.jsonl -c $CKAN_INI
+fi
+
+# Topics
+if [ "$groups_len" -lt 5 ]; then
+  ckanapi load groups -I /tmp/fixtures/topics.jsonl -c $CKAN_INI
+fi
+
+# Datasets
+if [ "$datasets_len" -lt 10 ]; then
+  ckanapi load datasets -I /tmp/fixtures/datasets.jsonl -p 4 -c $CKAN_INI
+fi
+
+# Resources
+# todo: sprinkle some example resources in some datasets
+
+
+# TODO: add a fake org and dataset with a csv resource for testing datatsore
